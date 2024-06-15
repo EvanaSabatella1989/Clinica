8c965c1043c36df349e61a96da9457c0e43a0ac4
sergioacos <arqsergioacosta@gmail.com>
2024-06-15 16:33:39 -0300

    se genero conexion con la bd

--- RegistrarPaciente.cs
+++ RegistrarPaciente.cs
@@ -20,7 +20,7 @@ namespace Clinica_SePrice
         private void btnGuardarPaciente_Click(object sender, EventArgs e)
         {
             string nombreIngresado = txtNombrePaciente.Text;
-            string apellidoIngresado = txtNombrePaciente.Text;
+            string apellidoIngresado = txtApellidoPaciente.Text;
             string dniIngresado = txtDniPaciente.Text;
 
             if (nombreIngresado == "" || apellidoIngresado == "" || dniIngresado == "")
@@ -29,14 +29,40 @@ namespace Clinica_SePrice
             }
             else
             {
-                if (dniIngresado == "12345678")
+                String respuesta;
+                Paciente paciente = new Paciente();
+                paciente.Nombre = txtNombrePaciente.Text;
+                paciente.Apellido = txtApellidoPaciente.Text;
+                paciente.Dni = Convert.ToInt32(txtDniPaciente.Text);
+                paciente.FechaNac = Convert.ToDateTime(dateTimePicker1.Text);
+
+              
+                respuesta = Datos.Pacientes.NuevoPaciente(paciente);
+                bool esnumero = int.TryParse(respuesta, out int codigo);
+                if (esnumero)
                 {
-                    MessageBox.Show("Ya existe un paciente registrado con ese DNI", "Registro Fallido", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    if (codigo == 0)
+                    {
+                        MessageBox.Show(" EL PACIENTE YA SE ENCUENTRA REGISTRADO", "AVISO DEL SISTEMA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Se almaceno con exito el paciente Nro " + respuesta, "AVISO DEL SISTEMA",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                        this.Close();
+                        //Form pagar = new Pagar();
+                        //pagar.ShowDialog();
+                    }
                 }
-                else
-                {
-                    MessageBox.Show("Registro exitoso", "Registro Exitoso", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
+                /* if (dniIngresado == "12345678")
+                 {
+                     MessageBox.Show("Ya existe un paciente registrado con ese DNI", "Registro Fallido", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                 }
+                 else
+                 {
+                     MessageBox.Show("Registro exitoso", "Registro Exitoso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                 }*/
             }
 
 
